1a26a9efceb0970371a9d680528fc1f057fc7c1d
kolirina <irinairin@list.ru>
2024-10-14 16:50:34 +0300

    feat: add text color

--- src/utils/username.js
+++ src/utils/username.js
@@ -1,4 +1,4 @@
-import { YELLOW_HIGHLIGHTED } from "./styleConstants.js";
+import { YELLOW_HIGHLIGHTED, YELLOW } from "./styleConstants.js";
 
 export function getUsername() {
   const args = process.argv.slice(2);
@@ -8,7 +8,7 @@ export function getUsername() {
       YELLOW_HIGHLIGHTED,
       `Dear friend, if you are using PowerShell, make sure you start with 'npm run start the app -- -- --username=your_name'. This is because of a bug in Node version 22 or higher.\nFor more information visit https://github.com/npm/cli/issues/7375\n`
     );
-    console.log(`Warning: Username not provided. Using 'Guest'.`);
+    console.log(YELLOW, `Warning: Username not provided. Using 'Guest'.`);
     return "Guest";
   }
   return usernameArg.split("=")[1];
